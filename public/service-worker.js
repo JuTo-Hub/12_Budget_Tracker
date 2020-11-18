f8fab7e5e8a8af0d398e1746e9ecdd4396ecec40
JuTo_Hub <JustinTorres1@Outlook.com>
2020-11-17 22:33:30 -0600

    Added db.js & manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -9,9 +11,6 @@ const FILES_TO_CACHE = [
     "/index.js"
   ];
   
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-    
   //install 
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
